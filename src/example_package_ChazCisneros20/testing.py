aa8271751d2eb283cc803be11393ef455154cf4d
ChazCisneros20 <chazcisneros@gmail.com>
2026-03-23 12:42:54 -0700

    Added branch logic for posts without descriptions. Cleaned up nicely for terminal

--- src/example_package_ChazCisneros20/testing.py
+++ src/example_package_ChazCisneros20/testing.py
@@ -7,38 +7,57 @@ example.capstone()
 
 import requests 
 import json
-URL = ""
+#CHECK TXT FILE FOR LINK
+URL = ''
 response = requests.get(url=URL)
 
 if response.status_code == 200:
     
     #Turns it into a dict.
     response_data = response.json()
-
-    #Data contains data. Other 2 branches are # of posts/comments, 
+    #Data contains data. Other 2 branches are # of posts/comments.
     response_data = response_data['data']
+    response_data = sorted(response_data, key=lambda item: item['score'], reverse=True)
     json_response_data = json.dumps(response_data, indent=4)
 
-    
-    #print(json_response_data)
-    
+    #===RAW DATA=== : Comment/Uncomment to refer to the full raw original data fetched
+    # print(json_response_data)
+
+    #===OLD METHOD===
+    # for item in response_data:
+    #     try:
+    #         print("[URL: ]", item['url'])
+    #         print("[TAG(S): ]", item["tag"])
+    #         print("[TITLE: ]", item['title'])
+    #         print("[SUBREDDIT: ]", item['subreddit'])
+    #         print("[AUTHOR DESCRIPTION: ]", item['author_description'], "\n")
+    #     #If there lies no post description (author_description) --> apply newline '\n'.                
+    #     except KeyError: 
+    #         print("\n")
+    #         continue
+
+    reddit_posts = '' 
+
+    #ADD: Send string variable to Ollama agent. 
+    #DOCUMENTATION: If there are no author descriptions, the loop logic should continue/loop over to next item.
+    #===CLEAN DATA===
     for item in response_data:
         try:
-            print(item['title'])
-        except KeyError:
+            reddit_posts += ("[URL: ] " + str(item['url']) + '\n' +
+                             "[SCORE: ]" + str(item['score']) + '\n' +
+                             "[TAG(s): ] " + str(item['tag']) + '\n' +
+                             "[TITLE: ] " + str(item['title']) + '\n' +
+                             "[SUBREEDDIT: ] " + str(item['subreddit']) + '\n')
+            reddit_posts += "[AUTHOR_DESCRIPTION: ]" + str(item['author_description']) + '\n' + '\n'
+        #If there lies no post description (author_description) --> apply newline '\n'.                
+        except KeyError: 
+            reddit_posts += '\n'
+            continue
 
-            try:
-                print(item['author_description'])
-                print(item['url'])
-            except KeyError:
-                continue
-
-            
-        
-    
+    print(reddit_posts)
 
 else: 
-    print('Response code not 200:', response.status_code)
+    print('Currently unable to search updates','<Response code not 200:>', response.status_code)
 
 
 #The user can answer android, sort, how many results.

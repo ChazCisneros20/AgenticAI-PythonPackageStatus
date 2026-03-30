13cd7e149e70cb48c880848e0db4775f08493ed7
ChazCisneros20 <chazcisneros@gmail.com>
2026-03-30 13:35:22 -0700

    Added ascending/descending parameter for main function. Added exception handling if ascending parameter is not boolean

--- src/PackageUpdateSearch/example.py
+++ src/PackageUpdateSearch/example.py
@@ -15,20 +15,36 @@ class example:
     def get_request(URL):
         response = requests.get(url=URL)
         print(response)
-    #FIXME:ADD .help() functio AND `help` command for CLI
+    #FIXME:ADD .help() function AND `help` command for CLI
 
     #===MAIN CAPSTONE FUNCTION; ANY UPDATES TODAY ==============================
+    #FIX: Ask prof if he wants it to return String or print(). 
+    #FIX: Ask if we are getting info for one specific package. Then I can model it accordingly. Is it dynamic for packages we choose?
+    #FIX: Ask if we the API has any parameters such as getting data from specific server, searching by keyword etc.
+    #So that I can implement a searchby= parameter that will be specific keywords OR if the API allows free searching. 
+    #searchby='r/apple' AND title='package'
     @staticmethod
-    def package_update(URL):
-        response = requests.get(url=URL)
+    def package_update(URL='', ascending=True):
 
+        #===ERROR HANDLING: `ascending` passed non-boolean value.
+        try:
+            if type(ascending) != bool:
+                raise TypeError
+        except TypeError:
+        #FIX:Ask professor if this is ok. Do we return string typically, do we want the user's backend to have a full error?
+            return TypeError("Function package_update() parameter `ascending` only takes boolean value")
+        
+
+        response = requests.get(url=URL)
+        
         if response.status_code == 200:
 
             #Turns it into a dict.
             response_data = response.json()
             #Data contains data. Other 2 branches are # of posts/comments.
             response_data = response_data['data']
-            response_data = sorted(response_data, key=lambda item: item['score'], reverse=True)
+            #`ascending` comes from the original function parameter. Default set to True (ASC order).
+            response_data = sorted(response_data, key=lambda item: item['score'], reverse=ascending)
             json_response_data = json.dumps(response_data, indent=4)
 
             reddit_posts = '' 
@@ -50,9 +66,17 @@ class example:
                     continue
                 
             return reddit_posts
-
         else: 
             return 'Currently unable to search updates <Response code not 200:>' + str(response.status_code)
+    
+    #===HELP FUNCTION for user===
+    @staticmethod
+    def help():
+        help = """
+TEST
+"""
+        return help
+
 
 
 
--- src/PackageUpdateSearch/testing.py
+++ src/PackageUpdateSearch/testing.py
@@ -1,56 +1,7 @@
-import requests 
-import json
-URL = ''
-response = requests.get(url=URL)
+# from PackageUpdateSearch.example import example
 
-if response.status_code == 200:
-    
-    #Turns it into a dict.
-    response_data = response.json()
-    #Data contains data. Other 2 branches are # of posts/comments.
-    response_data = response_data['data']
-    response_data = sorted(response_data, key=lambda item: item['score'], reverse=True)
-    json_response_data = json.dumps(response_data, indent=4)
+# print(example.package_update(ascending="Hello"))
 
-    #===RAW DATA=== : Comment/Uncomment to refer to the full raw original data fetched
-    # print(json_response_data)
+from example import example
 
-    #===OLD METHOD===
-    # for item in response_data:
-    #     try:
-    #         print("[URL: ]", item['url'])
-    #         print("[TAG(S): ]", item["tag"])
-    #         print("[TITLE: ]", item['title'])
-    #         print("[SUBREDDIT: ]", item['subreddit'])
-    #         print("[AUTHOR DESCRIPTION: ]", item['author_description'], "\n")
-    #     #If there lies no post description (author_description) --> apply newline '\n'.                
-    #     except KeyError: 
-    #         print("\n")
-    #         continue
-
-    reddit_posts = '' 
-
-    #ADD: Send string variable to Ollama agent. 
-    #DOCUMENTATION: If there are no author descriptions, the loop logic should continue/loop over to next item.
-    #===CLEAN DATA===
-    for item in response_data:
-        try:
-            reddit_posts += ("[URL: ] " + str(item['url']) + '\n' +
-                             "[SCORE: ]" + str(item['score']) + '\n' +
-                             "[TAG(s): ] " + str(item['tag']) + '\n' +
-                             "[TITLE: ] " + str(item['title']) + '\n' +
-                             "[SUBREEDDIT: ] " + str(item['subreddit']) + '\n')
-            reddit_posts += "[AUTHOR_DESCRIPTION: ]" + str(item['author_description']) + '\n' + '\n'
-        #If there lies no post description (author_description) --> apply newline '\n'.                
-        except KeyError: 
-            reddit_posts += '\n'
-            continue
-
-    print(reddit_posts)
-
-else: 
-    print('Currently unable to search updates','<Response code not 200:>', response.status_code)
-
-
-#The user can answer android, sort, how many results.
-#That value. 
+print(example.package_update(URL=r"https://releasetrain.io/api/reddit/query/positive", ascending=10))
